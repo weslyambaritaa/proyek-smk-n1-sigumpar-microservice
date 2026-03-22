50e0c1721aec542c9279669d7b8f844b11103423
wesly <weslyambarita4@gmail.com>
2026-03-22 13:11:54 +0700

    update

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -3,7 +3,7 @@ import Keycloak from 'keycloak-js';
 const keycloakConfig = {
   url: import.meta.env.VITE_KEYCLOAK_URL || 'http://localhost:8080',
   realm: import.meta.env.VITE_KEYCLOAK_REALM || 'smk-sigumpar',
-  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || 'frontend-client'
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || 'smk-sigumpar'
 };
 
 const keycloak = new Keycloak(keycloakConfig);
