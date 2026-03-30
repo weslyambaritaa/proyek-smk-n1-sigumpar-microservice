d0716ac91cc348f6c16d31d363ca2f8ae07d2eae
ivanapasaribu5 <ivanapasaribu17@gmail.com>
2026-03-30 10:44:39 +0700

    update

--- backend/learning-service/start.sh
+++ backend/learning-service/start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-service postgresql start
-sleep 5
 
-sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';" || true
-sudo -u postgres psql -c "CREATE DATABASE $DB_NAME OWNER $DB_USER;" || true
-sudo -u postgres psql -d $DB_NAME -f ./init.sql || true
-
-# Pastikan user punya akses penuh
-sudo -u postgres psql -d $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_USER;" || true
-sudo -u postgres psql -d $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $DB_USER;" || true
-
+# connection to external database via env DB_HOST etc
 npm run dev
