9c022fff5614356f4f819c6e54d467b663dfac79
lolagracea <142556094+lolagracea@users.noreply.github.com>
2026-05-01 22:42:20 +0700

    up

--- proyek-smk-n1-sigumpar-microservice-main/frontend/src/api/academicApi.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from './axiosInstance';
-
-// CRUD SISWA
-export const getStudents = () => axiosInstance.get('/academic/students');
-export const createStudent = (data) => axiosInstance.post('/academic/students', data);
-
-// CRUD KELAS
-export const getClasses = () => axiosInstance.get('/academic/classes');
-export const createClass = (data) => axiosInstance.post('/academic/classes', data);
